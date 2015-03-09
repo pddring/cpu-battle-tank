ffb72010bbad28593d5aa8fb68fb2b5132c54f5b
Pete Dring <pddring@googlemail.com>
2015-03-09 20:52:09 +0000

    Added info link

--- levels/l2.js
+++ levels/l2.js
@@ -1,15 +1,13 @@
 bt.addLevel({
 	controller: bt.CONTROLLER_LITTLE_MAN_COMPUTER, 
 	tank: {
-		x:1, y:1, 
+		x:5, y:5, 
 		name: "You", color:"rgba(0, 255,0,0.8)", 
-		code:"Ly8gTERBIGxvYWRzIGEgdmFsdWUgaW50byB0aGUgYWNjdW11bGF0b3IgcmVnaXN0ZXINCi8vIE9VVCBzZW5kcyB0aGUgdmFsdWUgaW4gdGhlIGFjY3VtdWxhdG9yIHJlZ2lzdGVyIHRvIGFuIG91dHB1dCBkZXZpY2UNCg0KLy8gc3RhcnQgb2YgaW5zdHJ1Y3Rpb25zDQoJTERBIEVhc3QNCglPVVQgMiAgICAgICAvLyBTdGVlciB0YW5rDQoNCglMREEgRm9yd2FyZHMNCglPVVQgMSAgICAgICAvLyBNb3ZlIHRhbmsNCglPVVQgMSAgICAgICAvLyBNb3ZlIHRhbmsNCglPVVQgMSAgICAgICAvLyBNb3ZlIHRhbmsNCglPVVQgMSAgICAgICAvLyBNb3ZlIHRhbmsNCg0KCUhMVCAgICAgICAgIC8vIFN0b3AgQ1BVIGJlZm9yZSBpdCBnZXRzIHRvIGRhdGENCg0KLy8gc3RhcnQgb2YgbWVtb3J5DQpFYXN0IERBVCAyICAgICAgLy8gc3RvcmUgMiBpbiB0aGlzIG1lbW9yeSBsb2NhdGlvbg0KRm9yd2FyZHMgREFUIDAgIC8vIHN0b3JlIDAgaW4gdGhpcyBtZW1vcnkgbG9jYXRpb24NCg=="
+		code:"Ly8gTW92ZSBmb3J3YXJkcw0KCUxEQSBGb3J3YXJkcyAgICAvLyBsb2FkIDAgaW50byBhY2N1bXVsYXRvcg0KCU9VVCAxICAgICAgICAgICAvLyBNb3ZlIGZvcndhcmRzDQoJT1VUIDENCglITFQgICAgICAgICAgICAgLy8gc3RvcCB0aGUgQ1BVDQoNCkZvcndhcmRzICAgIERBVCAwICAgICAgICAgICAvLyBzdG9yZSB0aGUgdmFsdWUgMA0K"
 		}, 
-	title: "Level 1", 
-	description:"Adapt the Little Man Computer program so that the tank avoids the landmine",
+	title: "Level 2", 
+	description:"Complete the Little Man Computer tank code to move as far as the checkpoint",
 	tasks:[
 		[5, 1, bt.ITEM_TYPE_CHECKPOINT, bt.ACTION_COLLECT],
-		[3, 1, bt.ITEM_TYPE_MINE, bt.ACTION_STATIC],
-		[3, 2, bt.ITEM_TYPE_CHECKPOINT, bt.ACTION_COLLECT],
 	]
 });
